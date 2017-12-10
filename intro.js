f7fb454fc92071ccd3c5bdb033aee81d739073da
Taylor Wong <taylorwong@Taylors-MacBook-Pro.local>
2017-12-10 14:25:27 -0500

    did button js for adding elements

--- intro.js
+++ intro.js
@@ -1,13 +1,23 @@
 // Two Soon: Samantha Ngo and Taylor Wong
 // SoftDev1 pd7
-// HW16 -- 
+// HW16 --
 // 2017-12-08
 
 var buttonCallBack = function(e){
-    console.log("This is ...");
-    console.log(e);
-    console.log("This is THIS Object...");
-    console.log(this);
+    //get current length of list
+    var currLength = document.getElementById('thelist').getElementsByTagName('li').length;
+    console.log(currLength);
+    //get current list
+    var list = document.getElementById('thelist');
+    //create new list element
+    var newElement = document.createElement('li');
+    //make list element say item nextnumber (next number being as long as list length)
+    newElement.innerHTML = 'item ' + currLength;
+    console.log(newElement);
+    //append the new element to the list
+    list.appendChild(newElement);
+    console.log(list);
+
 }
 
 var b = document.getElementById("b");
